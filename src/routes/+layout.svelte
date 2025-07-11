929619b2cd0a8b86b0c88a79cdf55077a28f7018
najongjine <najongjin3@hotmail.com>
2025-07-11 11:25:13 +0900

    .

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <!-- src/routes/+layout.svelte -->
 <script lang="ts">
+  import "../app.css"; // 경로가 맞는지 확인!
   import Header from '$lib/components/Header.svelte';
   import Footer from '$lib/components/Footer.svelte';
 </script>
